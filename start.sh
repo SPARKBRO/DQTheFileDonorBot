c4df1de298d289dd5654157401d4641f983a58e0
Midhun <103011794+SPARKBRO@users.noreply.github.com>
2023-07-17 12:55:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/SPARKBRO/DQTheFileDonorBot /DQTheFileDonorBotot
+  git clone https://github.com/SPARKBRO/DQTheFileDonorBot /DQTheFileDonorBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /DQTheFileDonorBot
